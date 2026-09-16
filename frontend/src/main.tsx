2b8fbf0b9a0ce3cf1379e72d46402a19dcd36bc3
Chaitanya Gatreddi <chaitanya.gatreddi@gmail.com>
2026-09-16 10:07:54 +0530

    Remove the question run mockup and its route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
-const wireframe = new URLSearchParams(window.location.search).get('wireframe')
-const { default: RootComponent } = wireframe === 'ideate'
-  ? await import('./IdeateWireframe.tsx')
-  : wireframe === 'review'
-    ? await import('./QuestionRunWireframe.tsx')
-    : await import('./App.tsx')
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RootComponent />
+    <App />
   </StrictMode>,
 )
